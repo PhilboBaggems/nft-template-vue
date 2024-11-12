2e35beb77ffdcf88ec5e65f5fb508c9f5a7c4741
PhilboBaggems <philbobagginzup@gmail.com>
2024-11-12 01:42:02 -0800

    Update production.ts
    I think I got it right

--- config/production.ts
+++ config/production.ts
@@ -1,10 +1,8 @@
-
+// Moonbase
 const config: ConfigInterface = {
   CHAIN_ID: '0x507',
   CONTRACT_ADDRESS: '0x4FE6d4d65682c07d2045f864adb6E8a41658ff63// Moonbase
-const config: ConfigInterface = {
-  CHAIN_ID: '0x507',
-  CONTRACT_ADDRESS: '',
+};
 };',
 };
 // Moonbeam
